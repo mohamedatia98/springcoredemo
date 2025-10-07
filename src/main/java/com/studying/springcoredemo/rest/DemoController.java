dfb09b37e61ba2b889a4525bf1bbf838ec57394d
mohamedatia98 <mohamedatia039@gmail.com>
2025-10-07 13:06:36 +0300

    Add @Qualifier annotation to constructor in DemoController for dependency injection

--- src/main/java/com/studying/springcoredemo/rest/DemoController.java
+++ src/main/java/com/studying/springcoredemo/rest/DemoController.java
@@ -19,7 +19,7 @@ public class DemoController {
 /* 
     // 1- (constructor injection) for dependency injection
     // @Autowired
-    public DemoController(Coach thecoach) {
+    public DemoController(@Qualifier("cricketCoach")Coach thecoach) {
         coach = thecoach;
     }
 */
