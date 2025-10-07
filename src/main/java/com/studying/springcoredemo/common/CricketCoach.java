87d0ea92f9ddddc7c328ea0e2bdd1e7bce36b4bd
mohamedatia98 <mohamedatia039@gmail.com>
2025-10-07 14:02:12 +0300

    Update CricketCoach workout duration and adjust DemoController's setCoach method for clarity

--- src/main/java/com/studying/springcoredemo/common/CricketCoach.java
+++ src/main/java/com/studying/springcoredemo/common/CricketCoach.java
@@ -1,14 +1,16 @@
 
 package com.studying.springcoredemo.common;
 
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 // here we create a Spring Bean
 @Component
+@Primary // here is for primary like @Qualifier("baseballCoach") but it will be used if no other bean is qualified
 public class CricketCoach implements Coach {
 
     @Override
     public String getDailyWorkout() {
-        return "Practice your spin bowling for 30 minutes.!!!!!!!!";
+        return "Practice your spin bowling for 11 minutes.!!!!!!!!";
     }
 }
