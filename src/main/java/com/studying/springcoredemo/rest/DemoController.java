f3921eff4555ac58d089e04ee8855e80ce893c99
mohamedatia98 <mohamedatia039@gmail.com>
2025-10-07 12:07:54 +0300

    Implement DemoController with dependency injection for Coach and add endpoint for daily workout

---
+++ src/main/java/com/studying/springcoredemo/rest/DemoController.java
@@ -0,0 +1,26 @@
+package com.studying.springcoredemo.rest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.studying.springcoredemo.common.Coach;
+
+
+
+@RestController
+public class DemoController {
+    // define private field for dependency injection
+    private Coach coach;
+
+    // constructor for dependency injection
+    // @Autowired
+    public DemoController(Coach thecoach) {
+        coach = thecoach;
+    }
+
+    @GetMapping("/workout")
+    public String getDailyWorkout() {
+        return coach.getDailyWorkout();
+    }
+}
