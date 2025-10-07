bd24ea10256fd59388d0ffd0765d212387c546ca
mohamedatia98 <mohamedatia039@gmail.com>
2025-10-07 11:59:50 +0300

    same two files but on another package

---
+++ src/main/java/untiltest/Coach.java
@@ -0,0 +1,6 @@
+package untiltest;
+
+
+public interface Coach {
+    String getDailyWorkout();
+}
---
+++ src/main/java/untiltest/CricketCoach.java
@@ -0,0 +1,13 @@
+package untiltest;
+
+import org.springframework.stereotype.Component;
+
+// here we create a Spring Bean
+@Component
+public class CricketCoach implements Coach {
+
+    @Override
+    public String getDailyWorkout() {
+        return "Practice your spin bowling for 30 minutes.!!!!!!!!";
+    }
+}
