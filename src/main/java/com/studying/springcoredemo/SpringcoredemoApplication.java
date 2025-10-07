b3d20bf1e2f799bd8468bfd2725c5c1186238092
mohamedatia98 <mohamedatia039@gmail.com>
2025-10-07 11:43:49 +0300

    Clean up SpringcoredemoApplication by removing unnecessary blank lines and comments & how to add other packages to be scanned by spring boot application

--- src/main/java/com/studying/springcoredemo/SpringcoredemoApplication.java
+++ src/main/java/com/studying/springcoredemo/SpringcoredemoApplication.java
@@ -3,7 +3,15 @@ package com.studying.springcoredemo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+// to scan other packages outside this package
 @SpringBootApplication
+
+
+// (scanBasePackages = {"com.studying.springcoredemo"})
+
+
+
 public class SpringcoredemoApplication {
 
 	public static void main(String[] args) {
