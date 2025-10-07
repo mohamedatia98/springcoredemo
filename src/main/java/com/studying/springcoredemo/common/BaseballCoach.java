2a5619b1165dc091bd63d6983a85fe6ec29e904d
mohamedatia98 <mohamedatia039@gmail.com>
2025-10-07 13:15:46 +0300

    Add BaseballCoach class implementing Coach interface with daily workout method

---
+++ src/main/java/com/studying/springcoredemo/common/BaseballCoach.java
@@ -0,0 +1,12 @@
+package com.studying.springcoredemo.common;
+import org.springframework.stereotype.Component;
+
+
+@Component
+public class BaseballCoach implements Coach {
+
+    @Override
+    public String getDailyWorkout() {
+        return "Spend 30 minutes on practice baseball";
+    }
+}
