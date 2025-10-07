558642bac1a4cc68dd65c2507dc22c7b422b071d
mohamedatia98 <mohamedatia039@gmail.com>
2025-10-07 15:02:02 +0300

    Add lifecycle methods to TennisCoach for initialization and cleanup

--- src/main/java/com/studying/springcoredemo/common/TennisCoach.java
+++ src/main/java/com/studying/springcoredemo/common/TennisCoach.java
@@ -2,8 +2,26 @@ package com.studying.springcoredemo.common;
 
 import org.springframework.stereotype.Component;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+
 @Component
 public class TennisCoach implements Coach {
+
+       public TennisCoach(){
+        System.out.println("TennisCoach: default constructor :" + getClass().getSimpleName());
+    }
+
+
+   @PostConstruct
+    public void doMyStartupStuff() {
+        System.out.println("TennisCoach:  doMyStartupStuff()");
+    }
+    
+    @PreDestroy
+    public void doMyCleanupStuff() {
+        System.out.println("TennisCoach:  doMyCleanupStuff()");
+    }
 
     @Override
     public String getDailyWorkout() {
