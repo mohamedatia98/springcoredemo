f494e433aea7af7a203b6864e3250e4614d4758f
mohamedatia98 <mohamedatia039@gmail.com>
2025-10-07 15:02:08 +0300

    Refactor DemoController to compare two Coach beans and update dependency injection method

--- src/main/java/com/studying/springcoredemo/rest/DemoController.java
+++ src/main/java/com/studying/springcoredemo/rest/DemoController.java
@@ -11,11 +11,18 @@ import com.studying.springcoredemo.common.Coach;
 
 @RestController
 public class DemoController {
+
+          // Logs for constructor  , just to note all beans are created at startup of the application .
+       public DemoController(){
+        System.out.println("DemoController: default constructor :" + getClass().getSimpleName());
+    }
+    
     // define private field for dependency injection
 
     // 3- if you want to use field injection make sure to use @Autowired on the field
     // @Autowired
     private Coach coach;
+        private Coach Newcoach;
 /* 
     // 1- (constructor injection) for dependency injection
     // @Autowired
@@ -25,14 +32,22 @@ public class DemoController {
 */
     // 2 - (Setter injection) set function for dependency injection
     @Autowired
-    public void setCoach(/*@Qualifier("baseballCoach")*/ Coach thecoach) {
+    public void setCoach(@Qualifier("cricketCoach") Coach thecoach , @Qualifier("cricketCoach")  Coach newcoach) {
         coach = thecoach;
+        Newcoach = newcoach;
     }
     
 // so using @Qualifier("baseballCoach") , will overwrite primary bean .
-
-    @GetMapping("/workout")
+// this code below does check between bean scope . 
+    @GetMapping("/check")
     public String getDailyWorkout() {
-        return coach.getDailyWorkout();
+       // return coach.getDailyWorkout();
+       return "Comparing Two Beans Now : " + coach.getDailyWorkout() + " vs " + Newcoach.getDailyWorkout() + " & " + (coach == Newcoach);
     }
+/* 
+    @GetMapping("/postbeans")
+    public String getPostBeans() {
+       return "Comparing Two Beans Now : " + coach.getPostConstruct() + " vs " + Newcoach.getPostConstruct();
+    }
+       */
 }
