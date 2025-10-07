7b145f46b81704f04565e43c9d3e46c511da8928
mohamedatia98 <mohamedatia039@gmail.com>
2025-10-07 11:18:46 +0300

    Refactor CricketCoach to implement Coach interface and define daily workout method

--- src/main/java/com/studying/springcoredemo/CricketCoach.java
+++ src/main/java/com/studying/springcoredemo/CricketCoach.java
@@ -1,5 +1,13 @@
 package com.studying.springcoredemo;
 
-public class CricketCoach {
+import org.springframework.stereotype.Component;
 
+// here we create a Spring Bean
+@Component
+public class CricketCoach implements Coach {
+
+    @Override
+    public String getDailyWorkout() {
+        return "Practice your spin bowling for 30 minutes.!!!!!!!!";
+    }
 }
