d4b1519fba46833e809c4bae6aacab23246a5581
mohamedatia98 <mohamedatia039@gmail.com>
2025-10-07 15:01:52 +0300

    Add lifecycle methods to BaseballCoach for initialization and cleanup

--- src/main/java/com/studying/springcoredemo/common/BaseballCoach.java
+++ src/main/java/com/studying/springcoredemo/common/BaseballCoach.java
@@ -1,9 +1,28 @@
 package com.studying.springcoredemo.common;
 import org.springframework.stereotype.Component;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+
 
 @Component
 public class BaseballCoach implements Coach {
+
+
+    public BaseballCoach(){
+        System.out.println("BaseballCoach: default constructor :" + getClass().getSimpleName());
+    }
+    
+        @PostConstruct
+    public void doMyStartupStuff() {
+        System.out.println("BaseballCoach:  doMyStartupStuff()");
+    }
+    
+    @PreDestroy
+    public void doMyCleanupStuff() {
+        System.out.println("BaseballCoach:  doMyCleanupStuff()");
+    }
+    
 
     @Override
     public String getDailyWorkout() {
