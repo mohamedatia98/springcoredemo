4641d2d8b3c8880c6dace052a4f1c34f165065ae
mohamedatia98 <mohamedatia039@gmail.com>
2025-10-07 12:00:08 +0300

    Refactor DemoController to enable dependency injection by adding missing imports

--- src/main/java/com/studying/springcoredemo/DemoController.java
+++ src/main/java/com/studying/springcoredemo/DemoController.java
@@ -1,7 +1,10 @@
 package com.studying.springcoredemo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import untiltest.Coach;
 
 @RestController
 public class DemoController {
@@ -9,6 +12,7 @@ public class DemoController {
     private Coach coach;
 
     // constructor for dependency injection
+    // @Autowired
     public DemoController(Coach thecoach) {
         coach = thecoach;
     }
