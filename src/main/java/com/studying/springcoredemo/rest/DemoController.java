aa6c3723b88db1968802676d47cb9c7bcb8efdd0
mohamedatia98 <mohamedatia039@gmail.com>
2025-10-07 12:59:15 +0300

    Refactor DemoController to clarify dependency injection methods and clean up commented code

--- src/main/java/com/studying/springcoredemo/rest/DemoController.java
+++ src/main/java/com/studying/springcoredemo/rest/DemoController.java
@@ -11,11 +11,20 @@ import com.studying.springcoredemo.common.Coach;
 @RestController
 public class DemoController {
     // define private field for dependency injection
-    private Coach coach;
 
-    // constructor for dependency injection
+    // 3- if you want to use field injection make sure to use @Autowired on the field
+    // @Autowired
+    private Coach coach;
+/* 
+    // 1- (constructor injection) for dependency injection
     // @Autowired
     public DemoController(Coach thecoach) {
+        coach = thecoach;
+    }
+*/
+    // 2 - (Setter injection) set function for dependency injection
+    @Autowired
+    public void setCoach(Coach thecoach) {
         coach = thecoach;
     }
 
